4d5750925c133f5d69f5b3aed5af053f1dbdfbb8
casperpj <casper@smuuth.dk>
2011-06-05 11:05:20 +0200

    Begun to sketch proper MODE handling

--- modules/mode.py
+++ modules/mode.py
@@ -11,19 +11,25 @@ module_config = {
 	"include channels":True
 }
 
-supported_modes = "nt"
+supported_modes = "ovnt"
 
 def handle_mode_request(client, channels, text):
-	try:
+#	try:
+#		mode_chunks = re.search("", text)
+		
 		mode_chunks = text.split()
 		target = mode_chunks[0]
 		try:
 			modes = mode_chunks[1]
+			try:
+				modes += " "+mode_chunks[2]
+			except:
+				pass
 		except:
 			modes = None
 		if modes is not None:
 			set_modes = []
-
+			print modes,"!!!!"
 			for mode in re.finditer("([\+\-])([a-z])(?: (?P<param>[a-zA-Z0-9]+))?", modes):
 				if mode.group(2) in supported_modes:
 					set_modes.append(mode.group(1)+mode.group(2))
@@ -40,7 +46,12 @@ def handle_mode_request(client, channels, text):
 						client.reply("ERR_UNKNOWNMODE", "You're not allowed to set mode for %s" % target)
 						return
 			for user in client.channels[target]["users"]:
-				user["client"].connection.send(":%s MODE %s :%s\n" % (client.nick, target, client.mode))
+				if mode.group("param") is not None:
+					for set_mode in set_modes:
+						user["client"].connection.send(":%s MODE %s :%s %s\n" % (client.nick, target, set_mode, mode.group("param")))
+				else:
+					for set_mode in set_modes:
+						user["client"].connection.send(":%s MODE %s :%s %s\n" % (client.nick, target, set_mode, mode.group("param")))
 		else:
 			if re.search("^#[a-zA-Z0-9_\-]{3,40}", target) is not None and target in client.channels:
 				channel_info = client.channels[target]
@@ -49,5 +60,5 @@ def handle_mode_request(client, channels, text):
 					user["client"].reply("329", "%s %i" % (target, int(time.time())), None) # created (now)
 			else:
 				client.reply("ERR_UNKNOWNMODE", "You're not on %s" % target)
-	except:
-		client.reply("ERR_UNKNOWNMODE", "Unknown mode")
+#	except:
+#		client.reply("ERR_UNKNOWNMODE", "Unknown mode")
